dab98df8318f2626cab579bb8bf1c3979ecbaf21
Derek Fenson <derekfenson@berkeley.edu>
2015-11-25 21:19:24 -0800

    update to main.py

--- code/main.py
+++ code/main.py
@@ -3,9 +3,15 @@ from rf_classify import *
 from cross_validation import *
 from datetime import datetime
 train_paths = document_paths("train")
+
+start = datetime.now()
 XX, WORDS, vectorizer = featurize_documents(train_paths)
+print datetime.now() - start
+
+start = datetime.now()
 X, words = lemmatize_design_matrix(XX, WORDS)
 X, words = remove_numerals(X, words)
+print datetime.now() - start
 
 y = get_labels(train_paths)
 
