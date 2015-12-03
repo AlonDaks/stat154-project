1dc93a97c70593ca559836df018756a4b6724747
Derek Fenson <derekfenson@berkeley.edu>
2015-12-02 20:25:55 -0800

    update to cross_validation

--- code/cross_validation.py
+++ code/cross_validation.py
@@ -1,19 +1,7 @@
 from load_data import *
-from sklearn.metrics import accuracy_score
-from sklearn.cross_validation import KFold, cross_val_score
-from numpy import mean
+from rf_cross_validate import *
 
-class CrossValidate:
-    def __init__(self, model, X, y):
-        self.model = model
-        self.X = X 
-        self.y = y
-    
-    def get_cv_score(self, cv):
-        scores = cross_val_score(self.model, self.X, self.y, cv = cv, n_jobs = -1)
-        return mean(scores)
-    
-    def set_model(self, model):
-        self.model = model
-        
+
+
+
     
